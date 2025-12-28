5c37731bb59c88a0eeb05ff6520917a1e1d30b38
= <=>
2025-12-28 14:47:24 +0700

    Membuat Project Dart (Commit 1)

---
+++ bin/jobsheet3_kombinasi.dart
@@ -0,0 +1,5 @@
+import 'package:jobsheet3_kombinasi/jobsheet3_kombinasi.dart' as jobsheet3_kombinasi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jobsheet3_kombinasi.calculate()}!');
+}
---
+++ test/jobsheet3_kombinasi_test.dart
@@ -0,0 +1,8 @@
+import 'package:jobsheet3_kombinasi/jobsheet3_kombinasi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
